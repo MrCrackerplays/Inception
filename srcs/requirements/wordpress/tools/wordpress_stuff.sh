145c1df6c54c7cd7b0e9c6407e3e3a04bc99ae86
pdruart <pdruart@student.codam.nl>
2023-08-22 17:39:18 +0200

    attempt fix wp-config either being overwritten or not being copied

--- srcs/requirements/wordpress/tools/wordpress_stuff.sh
+++ srcs/requirements/wordpress/tools/wordpress_stuff.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-if [ -f ./wp-config.php ]; then
-	echo "wp-config.php already downloaded"
+if [ -f ./download-complete ]; then
+	echo "wordpress already downloaded"
 else
-	echo creating new wp-config.php for $DOMAIN_NAME
+	echo downloading and extracting wordpress for $DOMAIN_NAME
 	wget http://wordpress.org/latest.tar.gz
 	tar -xzvf latest.tar.gz
 	rm latest.tar.gz
 	mv wordpress/* .
 	rm -rf wordpress
+	touch download-complete
 
+	# this should never actually replace anything as the premade wp-config.php is filled in
+	# but just in case moving latest's contents ever overwrites that file I'll leave this here
 	# s/STUFF/NEWSTUFF/g replace all occurences of STUFF with NEWSTUFF
 	sed -i "s/username_here/$WORDPRESS_DB_USER/g" wp-config.php
 	sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/g" wp-config.php
