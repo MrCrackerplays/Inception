3ae44e5ba5f36c03714c58abd6d9f20b346ba096
pdruart <pdruart@student.codam.nl>
2023-08-24 15:33:56 +0200

    overthowing a bunch of script stuff as shown by bence

--- srcs/requirements/mariadb/tools/mariadb_stuff.sh
+++ srcs/requirements/mariadb/tools/mariadb_stuff.sh
@@ -1,14 +1,32 @@
 #!/bin/sh
 
-mysql_install_db --user=root --basedir=/usr --datadir=/var/lib/mysql
-mysqld --user=root --datadir=/var/lib/mysql & sleep 4
+mysql_install_db
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY \`${MYSQL_PASSWORD}\`;"
-mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@\`%\` IDENTIFIED BY \`${MYSQL_PASSWORD}\`;"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY \`${MYSQL_ROOT_PASSWORD}\`;"
-mysql -e "FLUSH PRIVILEGES;"
+mysql start
 
-pkill mysqld
+#Check if the database exists
+
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
+then
+	echo "Database already exists"
+else
+
+mysql_secure_installation <<_EOF_
+
+Y
+$MYSQL_ROOT_PASSWORD
+$MYSQL_ROOT_PASSWORD
+Y
+n
+Y
+Y
+_EOF_
+
+#Create database and user for wordpress
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+
+fi
+
+mysql stop
 
 exec "$@"
--- srcs/requirements/wordpress/tools/wordpress_stuff.sh
+++ srcs/requirements/wordpress/tools/wordpress_stuff.sh
@@ -1,25 +1,20 @@
 #!/bin/sh
 
-if [ -f /usr/local/bin/wp-cli ]; then
-	echo "wp-cli already installed"
-else
-	echo installing wp-cli
-	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-	chmod +x wp-cli.phar
-	mv wp-cli.phar /usr/local/bin/wp-cli
-fi
-
 # installing wordpress via the cli instead of wget as I couldn't figure out how to set up the title n stuff otherwise
 if [ -f ./wp-config.php ]; then
 	echo "wordpress already installed"
 else
 	echo installing wordpress
 	/usr/local/bin/wp-cli core download --allow-root --path="/var/www/html"
-	rm -rf wp-config.php
-	cp /usr/local/bin/wp-config.php .
-	/usr/local/bin/wp-cli core install --allow-root --path="/var/www/html" --url=${DOMAIN_NAME} --title="pdruart's magic palace" --admin_user=${ADMIN_NAME} --admin_password=${ADMIN_PASSWORD} --admin_email=${ADMIN_EMAIL} --skip-email
+	# rm -rf wp-config.php
+	# cp /usr/local/bin/wp-config.php .
+	/usr/local/bin/wp-cli db create --allow-root
+	/usr/local/bin/wp-cli config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOSTNAME --allow-root
+	/usr/local/bin/wp-cli core install --allow-root --path="/var/www/html" --url=${DOMAIN_NAME} --title="pdruart's magic palace" --admin_user=${ADMIN_NAME} --admin_password=${ADMIN_PASSWORD} --admin_email=${ADMIN_EMAIL}
 	# regular user
-	/usr/local/bin/wp-cli user create --allow-root ${USER_NAME} ${USER_EMAIL} --role=author --user_pass=${USER_PASSWORD}
+	/usr/local/bin/wp-cli user create ${USER_NAME} ${USER_EMAIL} --role=editor --user_pass=${USER_PASSWORD} --allow-root
+	#permissions
+	chmod 600 wp-config.php
 fi
 
 exec "$@"
