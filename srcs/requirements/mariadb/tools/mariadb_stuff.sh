981f4f26dfb0d2b5a9c611ed73bd93cdac3c2e48
pdruart <pdruart@student.codam.nl>
2023-08-29 18:05:48 +0200

    magic I don't know it's a lot at once

--- srcs/requirements/mariadb/tools/mariadb_stuff.sh
+++ srcs/requirements/mariadb/tools/mariadb_stuff.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
-mysql_install_db
-
-mysql start
-
 #Check if the database exists
-
 if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
 then
-	echo "Database already exists"
+        echo "Database already exists"
 else
+
+mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+
+chown -R mysql /var/lib/mysql
+
+#we start early and stop it because exec seems to stop the rest of the script (it takes over the process?)
+#mysql start
+nohup /usr/bin/mariadbd-safe --user=root --datadir=/var/lib/mysql > /dev/null &
+bg_pid=$!
+
+sleep 5
 
 mysql_secure_installation <<_EOF_
 
+Y
 Y
 $MYSQL_ROOT_PASSWORD
 $MYSQL_ROOT_PASSWORD
@@ -22,11 +29,20 @@ Y
 Y
 _EOF_
 
+sleep 5
+
 #Create database and user for wordpress
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+#echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;"
+#echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+echo "GRANT ALL ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;"
+echo "GRANT ALL ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
 
+echo "miele miele miele miel"
+
+#mysql stop
+kill $bg_pid
+
+sleep 2
 fi
-
-mysql stop
 
 exec "$@"
--- srcs/requirements/wordpress/tools/wordpress_stuff.sh
+++ srcs/requirements/wordpress/tools/wordpress_stuff.sh
@@ -1,43 +1,59 @@
 #!/bin/sh
 
-if [ -f ./download-complete ]; then
-	echo "wordpress already downloaded"
-else
-	echo downloading and extracting wordpress for $DOMAIN_NAME
-	wget http://wordpress.org/latest.tar.gz
-	tar -xzvf latest.tar.gz
-	rm latest.tar.gz
-	mv wordpress/* .
-	rm -rf wordpress
-	touch download-complete
+sleep 25
 
-	rm -rf wp-config.php
-	cp /usr/local/bin/wp-config.php .
-	# this should never actually replace anything as the premade wp-config.php is filled in
-	# but just in case moving latest's contents ever overwrites that file I'll leave this here
-	# s/STUFF/NEWSTUFF/g replace all occurences of STUFF with NEWSTUFF
-	sed -i "s/username_here/$WORDPRESS_DB_USER/g" wp-config.php
-	sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/g" wp-config.php
-	sed -i "s/database_name_here/$WORDPRESS_DB_NAME/g" wp-config.php
-	sed -i "s/localhost/$DOMAIN_NAME/g" wp-config.php
+if [ -f ./download-complete ]; then
+        echo "wordpress already downloaded"
+else
+        echo downloading and extracting wordpress for $DOMAIN_NAME
+        wget http://wordpress.org/latest.tar.gz
+        tar -xzvf latest.tar.gz
+        rm latest.tar.gz
+        mv wordpress/* .
+        rm -rf wordpress
+        touch download-complete
+
+#       rm -rf wp-config.php
+#       cp /usr/local/bin/wp-config.php .
+#       # this should never actually replace anything as the premade wp-config.php is filled in
+#       # but just in case moving latest's contents ever overwrites that file I'll leave this here
+#       # s/STUFF/NEWSTUFF/g replace all occurences of STUFF with NEWSTUFF
+#       sed -i "s/username_here/$WORDPRESS_DB_USER/g" wp-config.php
+#       sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/g" wp-config.php
+#       sed -i "s/database_name_here/$WORDPRESS_DB_NAME/g" wp-config.php
+#       sed -i "s/localhost/$DOMAIN_NAME/g" wp-config.php
 fi
 
 
 # # installing wordpress via the cli instead of wget as I couldn't figure out how to set up the title n stuff otherwise
-# if [ -f ./wp-config.php ]; then
-# 	echo "wordpress already installed"
-# else
-# 	echo installing wordpress
-# 	wp core download --allow-root --path="/var/www/html"
-# 	# rm -rf wp-config.php
-# 	# cp /usr/local/bin/wp-config.php .
-# 	wp db create --allow-root
-# 	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOSTNAME --allow-root
-# 	wp core install --allow-root --path="/var/www/html" --url=${DOMAIN_NAME} --title="the magic palace of pdruart" --admin_user=${ADMIN_NAME} --admin_password=${ADMIN_PASSWORD} --admin_email=${ADMIN_EMAIL}
-# 	# regular user
-# 	wp user create ${USER_NAME} ${USER_EMAIL} --role=editor --user_pass=${USER_PASSWORD} --allow-root
-# 	#permissions
-# 	chmod 600 wp-config.php
-# fi
+ if [ -f ./wp-config.php ]; then
+        echo "wordpress already installed"
+ else
+        echo installing wordpress
+#       wp core download --allow-root --path="/var/www/html"
+#       rm -rf wp-config.php
+#       cp /usr/local/bin/wp-config.php .
+        echo "pre config create"
+        wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --allow-root --skip-check
+        echo "post ${WORDPRESS_DB_NAME} ${WORDPRESS_DB_USER} ${WORDPRESS_DB_PASSWORD} ${WORDPRESS_DB_HOST}"
+        sleep 1
+        echo "pre db create"
+        wp db create --allow-root
+        echo "post"
+        sleep 1
+        echo "pre core install"
+        wp core install --allow-root --path="/var/www/html" --url=${DOMAIN_NAME} --title="the magic palace of pdruart" --admin_user=${ADMIN_NAME} --admin_password=${ADMIN_PASSWORD} --admin_email=${ADMIN_EMAIL}
+        echo "post"
+        sleep 1
+        # regular user
+        echo "pre user install"
+        wp user create ${USER_NAME} ${USER_EMAIL} --role=editor --user_pass=${USER_PASSWORD} --allow-root
+        echo "post"
+        sleep 1
+        #permissions
+        echo "perms"
+        chmod 644 wp-config.php
+        echo "post"
+fi
 
 exec "$@"
